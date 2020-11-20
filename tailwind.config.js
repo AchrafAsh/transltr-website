e6509d683f23843a26c82db7fa1d56138329cbf5
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-11-20 11:35:32 +0100

    purge unused tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    purge: [],
+    purge: {
+        enable: true,
+        content: ['./pages/**/*.tsx']
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         colors: {
